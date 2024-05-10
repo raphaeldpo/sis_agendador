6852f018dee9f856b71e48ec2fd41ead7d18f8a1
Raphael Anderson <137456116+raphaeldpo@users.noreply.github.com>
2024-05-10 15:56:12 -0300

    Add files via upload

---
+++ sis_agendador/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SisAG 1.0</title>
+</head>
+<body>
+    <header> 
+        <h1>Sistema Agendador 1.0</h1>
+        <nav>
+            <a href="#">Home</a> |
+            <a href="#">Contato</a> |
+            <a href="#">Tarefas</a> |
+            <a href="#">Eventos</a>
+        </nav>
+    </header>
+    <main>
+       <?php
+            include("/pages/home/home.php");
+       ?>
+    </main>
+        
+</body>
+</html>
